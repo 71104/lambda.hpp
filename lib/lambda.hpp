a02ea1b1198932989401ee109177a7bdc3977e66
Alberto La Rocca <a71104@gmail.com>
2014-05-03 15:15:27 +0200

    Unused

--- lib/lambda.hpp
+++ lib/lambda.hpp
@@ -2,6 +2,11 @@
 #define __LAMBDA_HPP__
 
 namespace Lambda {
+	struct Unused {
+		template<typename... _Arguments>
+		Unused(_Arguments&...) {}
+	};
+
 	template<unsigned int const _i, typename ..._Types>
 	struct FindType {
 		typedef typename FindType<_i - 1, _Types...>::Type Type;
@@ -34,17 +39,6 @@ namespace Lambda {
 		}
 	};
 
-	static Bind<0> _1;
-	static Bind<1> _2;
-	static Bind<2> _3;
-	static Bind<3> _4;
-	static Bind<4> _5;
-	static Bind<5> _6;
-	static Bind<6> _7;
-	static Bind<7> _8;
-	static Bind<8> _9;
-	static Bind<9> _10;
-
 	template<typename _Operand>
 	struct UnaryFunctor :
 		public Functor
@@ -57,5 +51,20 @@ namespace Lambda {
 		}
 	};
 }
+
+static Lambda::Bind<0> _1;
+static Lambda::Bind<1> _2;
+static Lambda::Bind<2> _3;
+static Lambda::Bind<3> _4;
+static Lambda::Bind<4> _5;
+static Lambda::Bind<5> _6;
+static Lambda::Bind<6> _7;
+static Lambda::Bind<7> _8;
+static Lambda::Bind<8> _9;
+static Lambda::Bind<9> _10;
+
+static Lambda::Unused g_Unused = Lambda::Unused(
+	_1, _2, _3, _4, _5, _6, _7, _8, _9, _10
+	);
 
 #endif
